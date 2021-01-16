7e9001b64aa0f5a01008bcfe0d5b6c295b473f93
Marisa Cassidy <marisancassidy@gmail.com>
2021-01-16 12:47:27 +0900

    Added views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:new, :create, :index, :show]
+  resources :cocktails, only: %i[new create index show destroy]
 end
